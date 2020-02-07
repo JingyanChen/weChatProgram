c30ec570718e78761b970381da532ac466a1a1c9
jy <xqchendream@163.com>
2020-02-07 16:48:33 +0800

    button和view的点击事件，以及如何在js文件中实现按键处理函数的方法，控件使用data-hi 和 data-qf 写入私有信息，通过e.currentTarget.dataset.qf 和 .hi获得私有信息

--- button/pages/index/index.js
+++ button/pages/index/index.js
@@ -1,8 +1,23 @@
 //index.js
 //获取应用实例
+var count =0;
 var param ={
   data:{
-    mytext: 'hello world',
+    clickMsg:'显示点击内容'
+  },
+  //参数e是返回的参数，比如何时被点击，谁被点击，被点击的位置是什么
+  clickMe : function(e){
+    console.log(e);
+    count++;
+    param.data.clickMsg = '显示点击内容' + e.currentTarget.id + '点击次数' + count;
+
+    this.setData(param.data);
+  },
+
+  doloading : function(e){
+    param.data.clickMsg = '按钮被按下 id = ' + e.currentTarget.id + 'datat-hi = '+ e.currentTarget.dataset.qf + '点击次数 ' + count;
+
+    this.setData(param.data);
   }
 };
 Page(param);
